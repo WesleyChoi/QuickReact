511d89e8c2f1e63f4c70e2411bb9c4fecf19fa0c
Wesley <wesleyhchoi@gmail.com>
2021-06-28 19:28:32 -0700

    added nav to return to main page

--- app/src/main/java/com/example/quickreact/CompassAgilitySelector.java
+++ app/src/main/java/com/example/quickreact/CompassAgilitySelector.java
@@ -1,5 +1,6 @@
 package com.example.quickreact;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,14 +10,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 public class CompassAgilitySelector extends AppCompatActivity {
+
+    private Button nav_back_to_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.compass_agility_selector);
 
+
+        nav_back_to_main = (Button) findViewById(R.id.nav_back_main);
+        nav_back_to_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                returnToDrillSelector();
+            }
+        });
 
 //        FloatingActionButton fab = findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -27,4 +39,11 @@ public class CompassAgilitySelector extends AppCompatActivity {
 //            }
 //        });
     }
+
+    public void returnToDrillSelector() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
 }
