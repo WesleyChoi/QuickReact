9caa879904e3b8f26b6e9c81a97c0327f3465c1b
Wesley <wesleyhchoi@gmail.com>
2021-06-30 11:03:50 -0700

    changed total set time increments

--- app/src/main/java/com/example/quickreact/CompassAgilityExecute.java
+++ app/src/main/java/com/example/quickreact/CompassAgilityExecute.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
 
 public class CompassAgilityExecute extends AppCompatActivity {
 
     private Button nav_back_to_agility_selector;
+    private int int_total_time;
+    private int int_prompt_frequency;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,17 @@ public class CompassAgilityExecute extends AppCompatActivity {
                 returnToCompassAgilitySelector();
             }
         });
+
+
+        // get total set time from selector screen
+
+//        NumberPicker.OnValueChangeListener() {
+//            public void onValueChange() {
+//
+//            }
+//        }
+
+        //get prompt frequency from selector screen
 
 
     }
--- app/src/main/java/com/example/quickreact/CompassAgilitySelector.java
+++ app/src/main/java/com/example/quickreact/CompassAgilitySelector.java
@@ -11,11 +11,17 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
 
 public class CompassAgilitySelector extends AppCompatActivity {
 
     private Button nav_back_to_main;
     private Button nav_compass_to_execute;
+
+    private String[] number_picker_vals;
+
+    private NumberPicker number_picker_total_time;
+    private NumberPicker number_picker_prompt_frequency;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +48,17 @@ public class CompassAgilitySelector extends AppCompatActivity {
             }
         });
 
+        // set min and max values for number_picker
+        number_picker_total_time = findViewById(R.id.set_time_picker);
+        number_picker_total_time.setMinValue(0);
+        number_picker_total_time.setMaxValue(13);
+        number_picker_vals = new String[] {"10", "15", "20", "25", "30", "35", "40", "45", "50", "55", "60", "65", "70", "75"};
+        number_picker_total_time.setDisplayedValues(number_picker_vals);
+
+        // set min and max values for prompt_frequency
+        number_picker_prompt_frequency = findViewById(R.id.prompt_frequency_picker);
+        number_picker_prompt_frequency.setMinValue(1);
+        number_picker_prompt_frequency.setMaxValue(15);
     }
 
     // executes intent instantiation
